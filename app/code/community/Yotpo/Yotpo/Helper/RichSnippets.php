3c912e7a077880b48e71c078aa97e9957b6c5699
Micha Zana <micha@yotpo.com>
2014-06-16 18:04:34 +0300

    rich snippet for magento

--- app/code/community/Yotpo/Yotpo/Helper/RichSnippets.php
+++ app/code/community/Yotpo/Yotpo/Helper/RichSnippets.php
@@ -1,39 +1,12 @@
 <?php
 
-class Yotpo_Yotpo_Helper_Data extends Mage_Core_Helper_Abstract
+class Yotpo_Yotpo_Helper_RichSnippets extends Mage_Core_Helper_Abstract
 {
-
     private $_config;
 
     public function __construct ()
     {
         $this->_config = Mage::getStoreConfig('yotpo');
-    }
-
-    public function showWidget($thisObj, $product = null, $print=true)
-    {
-        $res = $this->renderYotpoProductBlock($thisObj, 'yotpo-reviews', $product, $print);
-        if ($print == false) {
-            return $res;
-        }
-    }
-
-    public function showBottomline($thisObj, $product = null, $print=true)
-    {
-
-        $res = $this->renderYotpoProductBlock($thisObj, 'yotpo-bottomline', $product);
-        if ($print == false){
-            return $res;
-        }
-    }
-
-    public function showQABottomline($thisObj, $product = null, $print=true)
-    {
-
-        $res = $this->renderYotpoProductBlock($thisObj, 'yotpo-qa-bottomline', $product);
-        if ($print == false){
-            return $res;
-        }
     }
 
     public function getRichSnippet()
@@ -84,36 +57,4 @@ class Yotpo_Yotpo_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return trim(Mage::getStoreConfig('yotpo/yotpo_general_group/yotpo_appkey',Mage::app()->getStore()));
     }
-
-    private function renderYotpoProductBlock($thisObj, $blockName, $product = null, $print=true)
-    {
-        $block = $thisObj->getLayout()->getBlock('content')->getChild('yotpo');
-        if ($block == null) {
-            Mage::log("can't find yotpo block");
-            return;
-        }
-
-        $block = $block->getChild($blockName);
-        if ($block == null) {
-            Mage::log("can't find yotpo child named: ".$blockName);
-            return;
-        }
-
-        if ($product != null)
-        {
-            $block->setAttribute('product', $product);
-        }
-        if ($block != null)
-        {
-
-            if ($print == true) {
-                echo $block->toHtml();
-            } else {
-                return $block->toHtml();
-            }
-
-        }
-
-    }
-
 }
