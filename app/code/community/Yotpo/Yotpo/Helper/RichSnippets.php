fca2a84ef641eab3c2e1674dbb7b4e83f0967797
Ijaz Ali <ijaz.ali@innovadeltech.com>
2017-11-30 18:38:09 +0500

    Standard MEQP1 spaces errors removed

--- app/code/community/Yotpo/Yotpo/Helper/RichSnippets.php
+++ app/code/community/Yotpo/Yotpo/Helper/RichSnippets.php
@@ -1,27 +1,25 @@
 <?php
 
-class Yotpo_Yotpo_Helper_RichSnippets extends Mage_Core_Helper_Abstract
-{
+class Yotpo_Yotpo_Helper_RichSnippets extends Mage_Core_Helper_Abstract {
+
     private $_config;
 
-    public function __construct ()
-    {
+    public function __construct() {
         $this->_config = Mage::getStoreConfig('yotpo');
     }
 
-    public function getRichSnippet()
-    {
-        
+    public function getRichSnippet() {
+
         try {
 
             $productId = Mage::registry('product')->getId();
             $snippet = Mage::getModel('yotpo/richsnippet')->getSnippetByProductIdAndStoreId($productId, Mage::app()->getStore()->getId());
-            
+
             if (($snippet == null) || (!$snippet->isValid())) {
                 //no snippet for product or snippet isn't valid anymore. get valid snippet code from yotpo api
-                
-                $res = Mage::helper('yotpo/apiClient')->createApiGet("products/".($this->getAppKey())."/richsnippet/".$productId, 2);
-                
+
+                $res = Mage::helper('yotpo/apiClient')->createApiGet("products/" . ($this->getAppKey()) . "/richsnippet/" . $productId, 2);
+
                 if ($res["code"] != 200) {
                     //product not found or feature disabled.
                     return "";
@@ -42,19 +40,18 @@ class Yotpo_Yotpo_Helper_RichSnippets extends Mage_Core_Helper_Abstract
                 $snippet->setReviewsCount($reviewsCount);
                 $snippet->setExpirationTime(date('Y-m-d H:i:s', time() + $ttl));
                 $snippet->save();
-                
-                return array( "average_score" => $averageScore, "reviews_count" => $reviewsCount);
-            }
-            return array( "average_score" => $snippet->getAverageScore(), "reviews_count" => $snippet->getReviewsCount());
 
-        } catch(Exception $e) {
+                return array("average_score" => $averageScore, "reviews_count" => $reviewsCount);
+            }
+            return array("average_score" => $snippet->getAverageScore(), "reviews_count" => $snippet->getReviewsCount());
+        } catch (Exception $e) {
             Mage::log($e);
         }
         return array();
     }
 
-    private function getAppKey()
-    {
-        return trim(Mage::getStoreConfig('yotpo/yotpo_general_group/yotpo_appkey',Mage::app()->getStore()));
+    private function getAppKey() {
+        return trim(Mage::getStoreConfig('yotpo/yotpo_general_group/yotpo_appkey', Mage::app()->getStore()));
     }
+
 }
