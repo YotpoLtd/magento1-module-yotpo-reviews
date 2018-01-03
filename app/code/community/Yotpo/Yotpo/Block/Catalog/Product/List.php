3846a800e263f2df5a2e0c65f5ad29db0f71f418
Ijaz Ali <ijaz.ali@innovadeltech.com>
2018-01-03 18:14:30 +0500

    extract conf string to constants Implemented.

--- app/code/community/Yotpo/Yotpo/Block/Catalog/Product/List.php
+++ app/code/community/Yotpo/Yotpo/Block/Catalog/Product/List.php
@@ -2,9 +2,10 @@
 
 class Yotpo_Yotpo_Block_Catalog_Product_List extends Mage_Catalog_Block_Product_List {
 
+	const CATEGORY_BOTTOMLINE = 'yotpo/yotpo_general_group/enable_bottomline_category_page';
     protected function _getProductCollection() {
         $_productCollection = parent::_getProductCollection();
-        $enableBottomlineCategoryPage = Mage::getStoreConfig('yotpo/yotpo_general_group/enable_bottomline_category_page');
+        $enableBottomlineCategoryPage = Mage::getStoreConfig(self::CATEGORY_BOTTOMLINE);
         if ($enableBottomlineCategoryPage) {
 
             foreach ($_productCollection as $_product) {
@@ -16,7 +17,7 @@ class Yotpo_Yotpo_Block_Catalog_Product_List extends Mage_Catalog_Block_Product_
 
     public function getReviewsSummaryHtml(Mage_Catalog_Model_Product $product, $templateType = false, $displayIfNoReviews = false) {
 
-        $enableBottomlineCategoryPage = Mage::getStoreConfig('yotpo/yotpo_general_group/enable_bottomline_category_page');
+        $enableBottomlineCategoryPage = Mage::getStoreConfig(self::CATEGORY_BOTTOMLINE);
         if ($enableBottomlineCategoryPage) {
             return $this->helper('yotpo')->showBottomline($this, $product);
         } else {
